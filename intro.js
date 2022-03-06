68bbb0b0aa175a5698a13a81ec1f1f5e4efcaf59
syabdull <syab5050@gmail.com>
2022-03-06 11:41:17 +0530

    js intro

--- intro.js
+++ intro.js
@@ -1,5 +1,15 @@
 
-let a="Abdulla";
-for(let i=1;i<=10;i++){
-    console.log(a);
-}
+// program  to check is prime no
+ let no =2;
+ isPrime=true;
+ for(let  i=2;i*i<=no;i++){
+     if(no%i==0){
+         isPrime=false;
+         break;
+     }
+ }
+ if(isPrime==true){
+     console.log("primeNo");
+ }else{
+     console.log("not a primeNo");
+ }
